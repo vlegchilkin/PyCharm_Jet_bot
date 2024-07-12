ec7ab48f5ccb03c75f1707a3dd8743ee1dfc97f8
Vitaly Legchilkin <vitaly.legchilkin@jetbrains.com>
2024-07-12 09:55:27 +0200

    add danger zones

--- bot.py
+++ bot.py
@@ -13,8 +13,7 @@ from vendeeglobe import (
     Vector,
     config,
 )
-from vendeeglobe.utils import distance_on_surface, longitude_difference, goto, wind_force
-
+from vendeeglobe.utils import distance_on_surface, goto, wind_force
 
 class Bot:
     """
@@ -24,69 +23,103 @@ class Bot:
     def __init__(self):
         self.team = "PyCharm JetBot"  # This is your team name
         self.step = 0
-        # This is the course that the ship has to follow
-        self.course = [
-            Checkpoint(latitude=46.8, longitude=-4.5, radius=50),
-            Checkpoint(latitude=58.7, longitude=-42.7, radius=50),
-            Checkpoint(latitude=59.7, longitude=-46.7, radius=50),
-            Checkpoint(latitude=60.7, longitude=-50, radius=50),
-            Checkpoint(latitude=73.9, longitude=-76.8, radius=50),
-            Checkpoint(latitude=74.341717, longitude=-93.926240, radius=50),
-            Checkpoint(latitude=73.8, longitude=-113.519828, radius=50),
-            Checkpoint(latitude=75.3, longitude=-122.296479, radius=50),
-            Checkpoint(latitude=74.7, longitude=-126.296479, radius=50),
-            Checkpoint(latitude=72.0, longitude=-130.0, radius=50),
-            Checkpoint(latitude=71.0, longitude=-130.5, radius=50),
-            Checkpoint(latitude=70.7, longitude=-135.8, radius=50),
-            Checkpoint(latitude=71.0, longitude=-144, radius=50),
-            Checkpoint(latitude=71.45, longitude=-159.5, radius=50),
-            Checkpoint(latitude=68.5, longitude=-168.0, radius=50),
-            Checkpoint(latitude=65.99, longitude=-168.3, radius=50),
-            Checkpoint(latitude=64.877243, longitude=-168.620675, radius=50),
+        self.course = []
+        self.zones = dict()
+        def zone(checkpoint, jiggle=45.0):
+            self.zones[len(self.course)] = jiggle
+            self.course += [checkpoint]
+            return checkpoint
 
-            Checkpoint(latitude=63.568421, longitude=-167.4, radius=50),
-            Checkpoint(latitude=62.568421, longitude=-167.948765, radius=50),
-            Checkpoint(latitude=60.4, longitude=-171, radius=50),
-            Checkpoint(latitude=51.839381, longitude=-179.35, radius=50),
-            Checkpoint(latitude=16, longitude=179.9, radius=50.0), # check 1
-            Checkpoint(latitude=14.2, longitude=169, radius=50.0),
-            Checkpoint(latitude=12.2, longitude=162, radius=50.0),
-            Checkpoint(latitude=9, longitude=152, radius=50.0),
-            Checkpoint(latitude=1, longitude= 128.8, radius=50.0),
-            Checkpoint(latitude=-2.5, longitude= 128.8, radius=50.0),
-            Checkpoint(latitude=-2.9, longitude=125.25, radius=50.0),
-            Checkpoint(latitude=-8.8, longitude=125.322399, radius=50.0),
-            Checkpoint(latitude=-9.241226, longitude=121.346855, radius=50.0),
-            Checkpoint(latitude=-9.2, longitude=116.866910, radius=50.0),
-            Checkpoint(latitude=-5.5, longitude=80, radius=50.0), # check 2
-            Checkpoint(latitude=12.5, longitude=51.0, radius=50.0),
-            Checkpoint(latitude=12.2, longitude=43.3, radius=50.0),
-            Checkpoint(latitude=29.9, longitude=32.3, radius=50.0),
-            Checkpoint(latitude=32.2, longitude=32.3, radius=50.0),
-            Checkpoint(latitude=37.2, longitude=11.193104, radius=50.0),
-            Checkpoint(latitude=38.0, longitude=10, radius=50.0),
-            Checkpoint(latitude=35.830124, longitude=-6.250658, radius=50.0),
-            Checkpoint(latitude=36.7, longitude=-10.5, radius=50.0),
-            Checkpoint(latitude=43.797109, longitude=-10.5, radius=50.0),
-            Checkpoint(
+        # This is the course that the ship has to follow
+        _ = [
+            zone(Checkpoint(latitude=46.8, longitude=-4.5, radius=50)),
+            zone(Checkpoint(latitude=58.7, longitude=-42.7, radius=50)),
+            zone(Checkpoint(latitude=59.7, longitude=-46.7, radius=50)),
+            zone(Checkpoint(latitude=60.7, longitude=-50, radius=50)),
+            zone(Checkpoint(latitude=73.9, longitude=-76.8, radius=50)),
+            zone(Checkpoint(latitude=74.341717, longitude=-93.926240, radius=50)),
+            zone(Checkpoint(latitude=73.8, longitude=-113.519828, radius=50)),
+            zone(Checkpoint(latitude=75.3, longitude=-122.296479, radius=50)),
+            zone(Checkpoint(latitude=74.7, longitude=-126.296479, radius=50)),
+            zone(Checkpoint(latitude=72.0, longitude=-130.0, radius=50)),
+
+            zone(Checkpoint(latitude=71.0, longitude=-130.5, radius=50)),
+            zone(Checkpoint(latitude=70.7, longitude=-135.8, radius=50)),
+            zone(Checkpoint(latitude=71.0, longitude=-144, radius=50)),
+            zone(Checkpoint(latitude=71.45, longitude=-159.5, radius=50)),
+            zone(Checkpoint(latitude=68.5, longitude=-168.0, radius=50)),
+            zone(Checkpoint(latitude=65.99, longitude=-168.3, radius=50)),
+            zone(Checkpoint(latitude=64.877243, longitude=-168.620675, radius=50)),
+
+            zone(Checkpoint(latitude=63.568421, longitude=-167.4, radius=50)),
+            zone(Checkpoint(latitude=62.568421, longitude=-167.948765, radius=50)),
+            zone(Checkpoint(latitude=60.4, longitude=-171, radius=50)),
+            zone(Checkpoint(latitude=51.839381, longitude=-179.35, radius=50)),
+            zone(Checkpoint(latitude=29, longitude=179.9, radius=50.0)),
+            zone(Checkpoint(latitude=16, longitude=179.9, radius=50.0)),  # check 1
+            zone(Checkpoint(latitude=14.2, longitude=169, radius=50.0)),
+            zone(Checkpoint(latitude=12.2, longitude=162, radius=50.0)),
+            zone(Checkpoint(latitude=9.2, longitude=152, radius=50.0)),
+            zone(Checkpoint(latitude=8, longitude=149.5, radius=50.0)),
+            zone(Checkpoint(latitude=6.7, longitude=146, radius=50.0)),
+            # entry
+            zone(Checkpoint(latitude=1, longitude=128.8, radius=50.0)),
+            zone(Checkpoint(latitude=-2.5, longitude=128.8, radius=50.0), 10),
+            zone(Checkpoint(latitude=-2.8, longitude=126.25, radius=50.0)),
+            zone(Checkpoint(latitude=-2.9, longitude=125.25, radius=50.0)),
+            zone(Checkpoint(latitude=-7.8, longitude=125.322399, radius=50.0)),
+            zone(Checkpoint(latitude=-8.8, longitude=125.322399, radius=50.0), 0),
+            zone(Checkpoint(latitude=-8.8, longitude=124.8, radius=50.0), 0),
+            zone(Checkpoint(latitude=-10, longitude=121.346855, radius=50.0)),
+            zone(Checkpoint(latitude=-11, longitude=120, radius=50.0)),
+            zone(Checkpoint(latitude=-9.5, longitude=106, radius=50.0)),
+            # Checkpoint(latitude=-9.2, longitude=116.866910, radius=50.0),
+            zone(Checkpoint(latitude=-5.5, longitude=80, radius=50.0)),  # check 2
+            zone(Checkpoint(latitude=-1, longitude=73, radius=50.0)),
+            zone(Checkpoint(latitude=11.5, longitude=52.0, radius=50.0)),
+            zone(Checkpoint(latitude=13, longitude=51.0, radius=50.0)),
+            #b-channel
+            zone(Checkpoint(latitude=11.8, longitude=44.3, radius=50.0)),
+            zone(Checkpoint(latitude=12.2, longitude=43.45, radius=50.0)),
+            zone(Checkpoint(latitude=14.2, longitude=42.3, radius=50.0), 0),
+
+            zone(Checkpoint(latitude=26.9, longitude=34.8, radius=50.0)),
+            zone(Checkpoint(latitude=27.6, longitude=34.0, radius=50.0), 10),
+            zone(Checkpoint(latitude=28.2, longitude=33.3, radius=50.0), 0),
+
+            zone(Checkpoint(latitude=29.9, longitude=32.3, radius=50.0), 0),
+            zone(Checkpoint(latitude=32.5, longitude=32.4, radius=50.0), 0),
+
+            zone(Checkpoint(latitude=34.5, longitude=23, radius=50.0)),
+            zone(Checkpoint(latitude=36.3, longitude=14.7, radius=50.0)),
+            zone(Checkpoint(latitude=37.2, longitude=12.3, radius=50.0)),
+
+            zone(Checkpoint(latitude=38.5, longitude=10, radius=50.0)),
+            zone(Checkpoint(latitude=38, longitude=4, radius=50.0)),
+            zone(Checkpoint(latitude=36.2, longitude=-2, radius=50.0)),
+            zone(Checkpoint(latitude=36, longitude=-4.8, radius=50.0)),
+
+            zone(Checkpoint(latitude=36, longitude=-6.25, radius=50.0), 0),
+            zone(Checkpoint(latitude=36.7, longitude=-10.5, radius=50.0)),
+            zone(Checkpoint(latitude=43.797109, longitude=-10.5, radius=50.0)),
+            zone(Checkpoint(
                 latitude=config.start.latitude,
                 longitude=config.start.longitude,
                 radius=5,
-            ),
+            )),
         ]
-        # self.course = self.course[24:]
 
     def run(
-        self,
-        t: float,
-        dt: float,
-        longitude: float,
-        latitude: float,
-        heading: float,
-        speed: float,
-        vector: np.ndarray,
-        forecast: Callable,
-        world_map: Callable,
+            self,
+            t: float,
+            dt: float,
+            longitude: float,
+            latitude: float,
+            heading: float,
+            speed: float,
+            vector: np.ndarray,
+            forecast: Callable,
+            world_map: Callable,
     ) -> Instructions:
         """
         This is the method that will be called at every time step to get the
@@ -136,12 +169,23 @@ class Bot:
         # Initialize the instructions
         instructions = Instructions()
 
-        # TODO: Remove this, it's only for testing =================
         current_position_forecast = forecast(
             latitudes=latitude, longitudes=longitude, times=0
         )
-        current_position_terrain = world_map(latitudes=latitude, longitudes=longitude)
+        # current_position_terrain = world_map(latitudes=latitude, longitudes=longitude)
         # ===========================================================
+        def heading(cp: Checkpoint, index: int) -> Heading:
+            angle = goto(Location(longitude, latitude), Location(longitude=cp.longitude, latitude=cp.latitude))
+            h = angle * np.pi / 180.0
+            ship_vector = np.array([np.cos(h), np.sin(h)])
+            wf = wind_force(ship_vector, np.array(current_position_forecast))
+            dist = np.linalg.norm(wf)
+            if dist < 8:
+                self.step = -1 if self.step == 1 else 1
+                jiggle = self.zones[index]
+                angle += self.step * jiggle
+
+            return Heading(angle)
 
         # Go through all checkpoints and find the next one to reach
         for idx, ch in enumerate(self.course):
@@ -165,15 +209,7 @@ class Bot:
             if dist < ch.radius:
                 ch.reached = True
             else:
-                angle = goto(Location(longitude, latitude), Location(longitude=ch.longitude, latitude=ch.latitude))
-                h = angle * np.pi / 180.0
-                ship_vector = np.array([np.cos(h), np.sin(h)])
-                wf = wind_force(ship_vector, np.array(current_position_forecast))
-                dist = np.linalg.norm(wf)
-                if dist < 8: #and idx in {1, 25}
-                    self.step = -1 if self.step == 1 else 1
-                    angle += self.step * 45
-                instructions.heading = Heading(angle)
+                instructions.heading = heading(ch, idx)
                 break
 
         return instructions
